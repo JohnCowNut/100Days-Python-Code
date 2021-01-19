0b704d68fc667bf76e743f134ca4c674fddeaecb
JohnCowNut <thuonghamno3xz@gmail.com>
2021-01-19 23:30:33 +0700

    done day2

---
+++ Day-2/lesson3.py
@@ -0,0 +1,14 @@
+print(round(8/3,2))
+print((8//3)) #floor
+
+result = 4 / 2 
+result /= 2
+print(result)
+
+score = 0
+
+
+score += 1
+print(score)
+# f'strign
+print(f"your score is {score}")
---
+++ Day-2/primitive-data-types.py
@@ -0,0 +1,14 @@
+# data type
+
+
+#String
+print("123" + "456")
+#Number
+print(123 + 456)
+
+#Float
+print(3.14)
+
+#Boolean
+True 
+False
