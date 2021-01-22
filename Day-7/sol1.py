fb7a03a320af8f461f625166b6206567430a04c7
JohnCowNut <thuonghamno3xz@gmail.com>
2021-01-22 22:41:57 +0700

    modified for loop at sol1 and add sol2 at step 2

--- Day-7/sol1.py
+++ Day-7/sol1.py
@@ -4,8 +4,8 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-idx_random = random.randint(0, 2)
-chosen_word = word_list[idx_random]
+
+chosen_word = random.choice(word_list)
 print('Choose word' + chosen_word)
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input('Guess a letter: ').lower()
